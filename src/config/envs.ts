5814abf8248d630be54f4127966e8954c616a30d
amoica <innovaweb.ar@gmail.com>
2025-04-25 17:14:18 -0300

    update_databaseUrl

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,13 +3,13 @@ import * as joi from 'joi';
 
 interface EnvVars{
     PORT_AUTH: number;
-    DATABASE_URL:string;
+    DATABASE_URL_AUTH:string;
     JWT_SECRET: string;
 }
 
 const envsSchema = joi.object({
     PORT_AUTH: joi.number().required(),
-    DATABASE_URL: joi.string().required(),
+    DATABASE_URL_AUTH: joi.string().required(),
     JWT_SECRET: joi.string().required(),
 })
 .unknown(true);
@@ -24,6 +24,6 @@ const EnvVars: EnvVars = value;
 
 export const envs = {
     port_auth: EnvVars.PORT_AUTH,
-    databaseUrl: EnvVars.DATABASE_URL,
+    databaseUrl: EnvVars.DATABASE_URL_AUTH,
     jwtSecret: EnvVars.JWT_SECRET
 }
